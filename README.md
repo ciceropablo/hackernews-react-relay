f7ae5c8c392c7a47a4fe410cdf5e19acfd2f128a
Cícero Pablo <cicero.pablos@gmail.com>
2017-07-26 14:14:32 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Hacker News with React + Relay
+
+> This is a [Hacker News](https://news.ycombinator.com/) application clone made using [React](https://facebook.github.io/react/), [Relay](https://facebook.github.io/relay/) and [GraphQL](http://graphql.org/).  
+To more information, access: [How to GraphQL](https://www.howtographql.com/).
---
+++ src/Environment.js
@@ -0,0 +1,45 @@
+import { GC_AUTH_TOKEN } from './constants'
+import { SubscriptionClient } from 'subscriptions-transport-ws'
+
+const {
+  Environment,
+  Network,
+  RecordSource,
+  Store,
+} = require('relay-runtime')
+
+const store = new Store(new RecordSource())
+
+const fetchQuery = (operation, variables) => {
+  return fetch('https://api.graph.cool/relay/v1/cj5af3yteldrn0105miu7hibq', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem(GC_AUTH_TOKEN)}`
+    },
+    body: JSON.stringify({
+      query: operation.text,
+      variables,
+    }),
+  }).then(response => {
+    return response.json()
+  })
+}
+
+const setupSubscription = (config, variables, cacheConfig, observer) => {
+  const query = config.text
+  const subscriptionClient = new SubscriptionClient('wss://subscriptions.graph.cool/v1/cj5af3yteldrn0105miu7hibq', {reconnect: true})
+  subscriptionClient.subscribe({query, variables}, (error, result) => {
+    observer.onNext({data: result})
+  })
+}
+
+const network = Network.create(fetchQuery, setupSubscription)
+
+const environment = new Environment({
+  network,
+  store
+})
+
+export default environment
